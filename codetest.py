946ac5ac3f27eb0be1859da0bfef21f19bfe05eb
westerhack <westerhack@gmail.com>
2016-06-09 11:55:41 -0700

    so it seems like i can create methods by doing

--- codetest.py
+++ codetest.py
@@ -1,25 +1,65 @@
-from overload
-if __name__ == '__main__':
-	class testclass():
-		def __init__(self, v):
-			self.v = v
-		@overload
-		def add(self, v):
-			self.v += v
-		def add1(self, v1, v2):
-			self.v += v1 * v2
-		add = overload(add1, 'add')
-	t = testclass(1)
-	print(t.add(9))
-	print(t.v)
-	# @overload
-	# def foo(a, b, c):
-	# 	print('foo(a,b): %s'%locals())
-	# def fooa(a, *args):
-	# 	print('no')
-	# foo = overload(fooa, 'foo', 0)
+from overload import *
+from types import MethodType as mt
+def q(s, v):
+	print(s, v)
+	s.v += v
+class tc():
+	def __init__(s, v): s.v = v
+	def __repr__(s): return type(s).__qualname__ + '({})'.format(s.v)
+	def a(s, v):
+		s.v += v
+	def a1(s, v1, v2):
+		s.v += v1 * v2
+t = tc(1)
+tc.a2 = mt(q, t)
+print(tc.a2(9))
+print(t.v)
 
-	# foo(1, 2, 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @overload
+# def foo(a, b, c):
+# 	print('foo(a,b): %s'%locals())
+# def fooa(a, *args):
+# 	print('no')
+# foo = overload(fooa, 'foo', 0)
+
+# foo(1, 2, 3)
 
 
 
